32754d23e6116ceec3a1211f8bb712e0338d9edb
nick <nickxylanmelloria@gmail.com>
2023-10-30 08:51:20 +0800

    added a win checker

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const gameBoard = (function() {
     const board = document.querySelectorAll('.board');
+    
+    let gameOver = false;
 
     const arrayBoard = [
         '','','',
         '','','',
         '','',''
-    ];
+    ];  
 
     board.forEach((board) => {
         board.addEventListener('click', () => {
-            if(arrayBoard[board.id] != '') {
-                return;
-            }
-            arrayBoard[board.id] = players.getPersonSign;
-            playRound();
+            playRound(board);
         });
     })
-    function playRound() {
-        update();
+    function playRound(board) {
+        if(arrayBoard[board.id] != '') {
+            return;
+        }
+        arrayBoard[board.id] = players.getPersonSign();
+        checkForWin(arrayBoard);
+        updateDom();
         getComputerChoice();
     }
-    function update() {
+    function updateDom() {
         for(let i = 0; i <= arrayBoard.length - 1; i++){
             board[i].innerHTML = arrayBoard[i];
         }
@@ -30,8 +33,43 @@ const gameBoard = (function() {
         if(arrayBoard[computerChoice] != '') {
             getComputerChoice();
         } else {
-            arrayBoard[computerChoice] = 'O';
-            update();
+            arrayBoard[computerChoice] = players.getComputerSign();
+            updateDom();
+            checkForWin(arrayBoard);
+        }
+    }
+    // checkForWin(arrayBoard);
+    function checkForWin(arrayBoard) {
+        const board = [
+            [],
+            [],
+            []
+        ];
+        //converts my 1d array into 2d array
+        let m = 0;
+        for (let k = 0; k < 3; k++) {
+            for (let l = 0; l < 3; l++) {
+                board[k][l] = arrayBoard[m]
+                m++;
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === board[i][j+1] && board[i][j] != ''
+                    && board[i][j+1] === board[i][j+2] && board[i][j+1] != '') {
+                        console.log(board[i][j], 'win ROW');
+                        return;
+                } else if (board[0][i] === board[1][i] && board[0][i] != ''
+                    && board[1][i] === board[2][i] && board[1][i] != '') {
+                        console.log(board[i][i], 'win COLUMN');
+                        return;
+                }
+            }
+        }
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] != ''
+            || board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[1][1] != '') {
+                console.log(board[1][1], 'win DIAGONAL');
+                return;
         }
     }
 })();
@@ -39,21 +77,21 @@ const gameBoard = (function() {
 const players = (function() {
     function Player(sign) {
         this.sign = sign;
-    }
-    Player.prototype.getPlayerSign = () => {
-    		return sign;
+        this.getPlayerSign = function() {
+            return sign;
+        }
     }
     
     const person = new Player('X');
     const computer = new Player('O');
     
-    const computerPlay = function() {
+    computer.computerPlay = function() {
         return Math.floor(Math.random() * 9);
     }
     return {
-        computerPlay: computerPlay,
+        computerPlay: computer.computerPlay,
         getPersonSign: person.getPlayerSign,
-        //computerSign: computer.sign
+        getComputerSign: computer.getPlayerSign,
     }
 })();
 
